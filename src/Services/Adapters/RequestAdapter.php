0434e482914da3f2844a7d1a17f9f1228a358548
felipe.pereira <felipelplope@gmail.com>
2019-10-21 11:45:25 -0300

    Create new field on Person about customer document country

--- src/Services/Adapters/RequestAdapter.php
+++ src/Services/Adapters/RequestAdapter.php
@@ -66,7 +66,8 @@ class RequestAdapter extends BaseAdapter
             'email' => $person->email,
             'phone_number' => $person->phoneNumber,
             'person_type' => $person->type,
-            'birth_date' => $person->birthdate,
+			'birth_date' => $person->birthdate,
+			'document_country' => $person->documentCountry,
         ];
     }
 
