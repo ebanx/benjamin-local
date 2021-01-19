0ec2e0cb28027bd2526f99a42d62cc30580115dd
Diego Miranda Neves Gomes <diego.miranda.ng@gmail.com>
2021-01-19 10:36:08 -0300

    [EBX-2274] Do not send card token and card info together

--- src/Services/Adapters/CardPaymentAdapter.php
+++ src/Services/Adapters/CardPaymentAdapter.php
@@ -20,13 +20,18 @@ class CardPaymentAdapter extends BrazilPaymentAdapter
 
     private function transformCard()
     {
-        return (object) [
+        $cardObject =  (object) [
             'card_number' => $this->payment->card->number,
             'card_name' => $this->payment->card->name,
             'card_due_date' => $this->payment->card->dueDate ? $this->payment->card->dueDate->format('m/Y') : null,
             'card_cvv' => $this->payment->card->cvv,
             'auto_capture' => $this->payment->card->autoCapture,
-            'token' => $this->payment->card->token
         ];
+
+        if (!$this->payment->card->createToken) {
+            $cardObject->token = $this->payment->card->token;
+        }
+
+        return $cardObject;
     }
 }
